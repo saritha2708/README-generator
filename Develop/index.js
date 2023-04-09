5b18db232ba602c8717cd39578ac253250e6bd53
Saritha Koduru <saritha.2708@gmail.com>
2023-04-08 21:44:32 -0700

    updated the functions in generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [{
     type: 'input',
     message: 'What is the title of the project? ',
@@ -56,14 +56,14 @@ const questions = [{
   },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data,(err) => {
         err ? console.error(err) : console.log('Success! Your README.md file has been generated.')
     });
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
     inquirer
     .prompt(questions)
